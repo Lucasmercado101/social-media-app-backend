7ba36bc910f8c2fe6f93bb94ddee709fbac64b9c
lucasmercado101 <lucasmercado101@gmail.com>
2021-05-31 09:25:44 -0300

    fixed passport login

--- src/routes/auth/login.js
+++ src/routes/auth/login.js
@@ -1,8 +1,15 @@
 const Router = require("express").Router;
-
+const passport = require("passport");
 const ROUTE = "/auth/login";
 
-module.exports = Router({ mergeParams: true }).get(
-	ROUTE,
-	async (req, res, next) => {}
+module.exports = Router({ mergeParams: true }).post(ROUTE, (req, res, next) =>
+  passport.authenticate("local", function (err, user) {
+    if (err) return res.sendStatus(500);
+    if (!user) return res.sendStatus(404);
+
+    req.login(user, function (err) {
+      if (err) return res.sendStatus(500);
+      return res.sendStatus(200);
+    });
+  })(req, res, next)
 );
