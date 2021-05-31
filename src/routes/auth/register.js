e923eff67bd8418b63893f98104fcdbd1069c934
lucasmercado101 <lucasmercado101@gmail.com>
2021-05-31 09:09:13 -0300

    added register route

---
+++ src/routes/auth/register.js
@@ -0,0 +1,17 @@
+const Router = require("express").Router;
+const { User } = require("../../db/models/index");
+const bcrypt = require("bcryptjs");
+
+const ROUTE = "/auth/register";
+
+module.exports = Router({ mergeParams: true }).post(ROUTE, async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ where: { username } });
+  if (user) return res.sendStatus(409);
+
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
+  const newUser = await User.create({ username, password: hashedPassword });
+  res.json(newUser);
+});
