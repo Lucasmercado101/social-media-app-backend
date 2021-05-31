4b376ba7e35be5e049bd02b4008edaf14ffd2b5a
lucasmercado101 <lucasmercado101@gmail.com>
2021-05-31 10:05:21 -0300

    trim content

--- src/routes/user/posts/addPost.js
+++ src/routes/user/posts/addPost.js
@@ -6,6 +6,8 @@ module.exports = Router({ mergeParams: true }).post(
   ROUTE,
   isLoggedIn,
   async (req, res) => {
+    //TODO: validation
+    req.body.content = req.body.content.trim();
     const newPost = await req.user.createPost(req.body);
     res.json(newPost);
   }
