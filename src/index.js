6eb472a035da02ce526251d06df456a842b457e2
lucasmercado101 <lucasmercado101@gmail.com>
2021-05-31 09:01:12 -0300

    added morgan, session and fixed rootrouter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 const express = require("express");
+const session = require("express-session");
 const db = require("./db/models/index");
 const app = express();
+const morgan = require("morgan");
+const rootRouter = require("./routes/createRouter");
 
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    name: "social-media-back-login-cookie"
+  })
+);
+
+app.use(morgan("dev"));
 app.use(express.json());
 
 db.sync({ force: true }).then(() => console.log("Synced successfully"));
 
-app.listen(5000, () => {
+app.use("/", rootRouter());
+
+app.listen(2000, () => {
   console.log("Port started successfully");
 });
