ac26a534f53866aab1337d50a24a5dc39c66954c
lucasmercado101 <lucasmercado101@gmail.com>
2021-06-06 10:34:41 -0300

    created

---
+++ src/routes/user/explore/getExplorePosts.js
@@ -0,0 +1,46 @@
+const Router = require("express").Router;
+const isLoggedIn = require("../../middleware/isLoggedIn");
+const { Post } = require("../../../db/models/index");
+
+const ROUTE = "/user/explore";
+
+module.exports = Router({ mergeParams: true }).get(
+  ROUTE,
+  isLoggedIn,
+  async (req, res) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const results = {};
+
+    const { count } = await Post.findAndCountAll();
+
+    if (endIndex < count) {
+      results.next = {
+        page: page + 1,
+        limit: limit
+      };
+    }
+
+    if (startIndex > 0 && startIndex < count) {
+      results.previous = {
+        page: page - 1,
+        limit: limit
+      };
+    }
+
+    try {
+      results.results = await Post.findAll({
+        offset: startIndex,
+        limit: limit
+        // where: {}
+      });
+      res.json(results);
+    } catch (e) {
+      res.sendStatus(500);
+    }
+  }
+);
