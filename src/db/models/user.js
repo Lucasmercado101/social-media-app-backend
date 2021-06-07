9033afec7c4b941749cec95fccd589a718e4cc2b
lucasmercado101 <lucasmercado101@gmail.com>
2021-06-07 09:29:57 -0300

    first and last name can be max 50 chars

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -3,10 +3,10 @@ const { DataTypes } = require("sequelize");
 module.exports = (db) =>
   db.define("User", {
     firstName: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING(50)
     },
     lastName: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING(50)
     },
     username: {
       type: DataTypes.STRING
