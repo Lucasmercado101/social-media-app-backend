7b18d2f248337883fb728f1bf3f79cb73369e59d
lucasmercado101 <lucasmercado101@gmail.com>
2021-06-07 19:43:01 -0300

    added profile pic url

--- src/routes/user/editMyInfo.validation.js
+++ src/routes/user/editMyInfo.validation.js
@@ -3,6 +3,7 @@ const Joi = require("joi");
 module.exports = {
   updateUserDataSchema: Joi.object({
     firstName: Joi.string().empty("").max(50).optional(),
-    lastName: Joi.string().empty("").max(50).optional()
+    lastName: Joi.string().empty("").max(50).optional(),
+    profilePictureURL: Joi.string().empty("").optional()
   })
 };
