944e9f63fe016c93fd5fdcaa398c9ffabe12f5a4
lucasmercado101 <lucasmercado101@gmail.com>
2021-06-07 09:01:48 -0300

    added first and last name

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -2,6 +2,12 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (db) =>
   db.define("User", {
+    firstName: {
+      type: DataTypes.STRING
+    },
+    lastName: {
+      type: DataTypes.STRING
+    },
     username: {
       type: DataTypes.STRING
     },
