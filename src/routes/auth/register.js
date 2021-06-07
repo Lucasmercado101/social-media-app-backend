a4c66825f7b5c21ff50f01b5dd80e6863e773742
lucasmercado101 <lucasmercado101@gmail.com>
2021-06-07 09:13:44 -0300

    fixed register

--- src/routes/auth/register.js
+++ src/routes/auth/register.js
@@ -5,13 +5,17 @@ const bcrypt = require("bcryptjs");
 const ROUTE = "/auth/register";
 
 module.exports = Router({ mergeParams: true }).post(ROUTE, async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password, ...otherProps } = req.body;
 
   const user = await User.findOne({ where: { username } });
   if (user) return res.sendStatus(409);
 
   const hashedPassword = bcrypt.hashSync(password, 10);
 
-  const newUser = await User.create({ username, password: hashedPassword });
+  const newUser = await User.create({
+    username,
+    password: hashedPassword,
+    ...otherProps
+  });
   res.json(newUser);
 });
