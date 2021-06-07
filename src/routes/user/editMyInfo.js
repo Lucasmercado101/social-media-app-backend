dfb713fa7552346b88296b9edc131ad8a50c9a1a
lucasmercado101 <lucasmercado101@gmail.com>
2021-06-07 09:43:57 -0300

    added validator logic

--- src/routes/user/editMyInfo.js
+++ src/routes/user/editMyInfo.js
@@ -1,15 +1,24 @@
 const Router = require("express").Router;
 const isLoggedIn = require("../middleware/isLoggedIn");
-
+const Joi = require("joi");
+const { updateUserDataSchema } = require("./editMyInfo.validation");
 const ROUTE = "/user";
 
 module.exports = Router({ mergeParams: true }).put(
   ROUTE,
   isLoggedIn,
   async (req, res) => {
+    let updateUserData;
+    try {
+      updateUserData = Joi.attempt(req.body, updateUserDataSchema);
+    } catch (e) {
+      if (e.isJoi) return res.status(400).send(e.message);
+      return res.sendStatus(500);
+    }
+
     for (let [key] of Object.entries(req.user.toJSON())) {
-      if (req.body[key]) {
-        req.user[key] = req.body[key];
+      if (updateUserData[key]) {
+        req.user[key] = updateUserData[key];
       }
     }
     await req.user.save();
