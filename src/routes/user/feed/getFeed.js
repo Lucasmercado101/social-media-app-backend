4991914d9d0e2842fbc7f1fdcf3ef81b5a379cce
Lucasmercado101 <lucasmercado101@gmail.com>
2021-06-12 13:27:50 -0300

    works

--- src/routes/user/feed/getFeed.js
+++ src/routes/user/feed/getFeed.js
@@ -1,5 +1,7 @@
 const Router = require("express").Router;
 const isLoggedIn = require("../../middleware/isLoggedIn");
+const { Post, User } = require("../../../db/models/index");
+const { Op } = require("sequelize");
 
 const ROUTE = "/user/feed";
 
@@ -7,7 +9,89 @@ module.exports = Router({ mergeParams: true }).get(
   ROUTE,
   isLoggedIn,
   async (req, res) => {
-    // res.json(await req.user.getUserPosts());
-    res.sendStatus(200);
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const results = {};
+
+    // console.log(Object.keys(Object.getPrototypeOf(req.user)));
+    // all posts from the user's friends and the user himself
+    const friendsIds = await req.user
+      .getFriends({
+        attributes: ["id"],
+        through: {
+          attributes: []
+        },
+        joinTableAttributes: []
+      })
+      .then((resp = []) => resp.map((friend) => friend.id));
+
+    const idsToCheck = [...friendsIds, req.user.id];
+
+    const { count } = await Post.findAndCountAll({
+      include: [
+        {
+          model: User,
+          where: {
+            id: { [Op.in]: idsToCheck }
+          }
+        }
+      ]
+    });
+
+    if (endIndex < count) {
+      results.next = {
+        page: page + 1,
+        limit: limit
+      };
+    }
+
+    if (startIndex > 0 && startIndex < count) {
+      results.previous = {
+        page: page - 1,
+        limit: limit
+      };
+    }
+
+    try {
+      results.results = await Post.findAll({
+        offset: startIndex,
+        limit: limit,
+        include: [
+          {
+            model: User,
+            attributes: ["firstName", "lastName", "profilePictureURL"],
+            where: {
+              id: { [Op.in]: idsToCheck }
+            }
+          },
+          {
+            model: User,
+            as: "likes",
+            attributes: ["id"],
+            through: {
+              attributes: []
+            },
+            distinct: true
+          },
+          {
+            model: User,
+            as: "dislikes",
+            attributes: ["id"],
+            through: {
+              attributes: []
+            }
+          }
+        ],
+        order: [["createdAt", "DESC"]]
+      });
+      res.json(results);
+    } catch (e) {
+      console.log(e);
+      res.sendStatus(500);
+    }
   }
 );
